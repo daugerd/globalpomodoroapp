1d095ec8bbafa1a8bc147b9ab4a9d8804d06ac49
Karolis Daugerdas <karolisdaugerdas@icloud.com>
2026-05-30 21:00:34 +0300

    Initial Commit

---
+++ Global Pomodoro/Global_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Global_PomodoroApp.swift
+//  Global Pomodoro
+//
+//  Created by Karolis Daugerdas on 30.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Global_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
