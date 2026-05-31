0c6b42637c2a8004cd66563cdd23d52dd9e175e3
Karolis Daugerdas <karolisdaugerdas@icloud.com>
2026-05-31 11:52:14 +0300

    initial app

--- Global Pomodoro/ContentView.swift
+++ Global Pomodoro/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            VStack {
+                TimelineView(.periodic(from: .now, by: 1.0)) { context in
+                        if(Calendar.current.component(.minute, from: Date()) < 25) {
+                        Image(systemName: "play.circle")
+                            .imageScale(.large)
+                            .foregroundStyle(.tint)
+                            } else if ((Calendar.current.component(.minute, from: Date()) >= 30 && Calendar.current.component(.minute, from: Date()) < 55)) {
+                                Image(systemName: "play.circle")
+                                    .imageScale(.large)
+                                    .foregroundStyle(.tint)
+                            } else {
+                                Image(systemName: "pause.circle")
+                                    .imageScale(.large)
+                                    .foregroundStyle(.tint)
+                            }
+                    
+                    
+                    
+                    Text(Date(), format: .dateTime.minute().second())
+                }
+            }
+            .padding()
+    }}
 
 #Preview {
     ContentView()
